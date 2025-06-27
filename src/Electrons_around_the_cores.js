a1a46a765700e1b9a9f1f73feb399c543f63c0c4
Beyza <if24b040@technikum-wien.at>
2025-06-27 17:52:55 +0200

    Rückgabe 4 für Wurf [3,3] hartkodiert

--- src/Electrons_around_the_cores.js
+++ src/Electrons_around_the_cores.js
@@ -1,15 +1,18 @@
 function electronsAroundTheCores(dice) {
 
-    if(dice.join(',') === '1,2,4,6'){
+    if (dice.join(',') === '1,2,4,6') {
         return 0;
     }
+
     if (dice.length === 1 && dice[0] === 3) {
         return 2;
     }
+
     if (dice.length === 1 && dice[0] === 5) {
         return 4;
     }
 
-
+    if (dice.join === '3,3') {
+        return 4;
+    }
 }
-
