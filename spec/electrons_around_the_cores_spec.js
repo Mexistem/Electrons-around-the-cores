966ff5f2b86b29e6bea6ed74be02dedde2c080b7
Beyza <if24b040@technikum-wien.at>
2025-06-27 17:48:35 +0200

    Test für Wurf [3,3] → 4 schreiben

--- spec/electrons_around_the_cores_spec.js
+++ spec/electrons_around_the_cores_spec.js
@@ -3,12 +3,16 @@ describe('electronsAroundTheCores', () => {
     expect(electronsAroundTheCores([3])).toBe(2);
   });
 
-  it ('Für einen einzigen Wurf mit 5 soll der Wert 4 zurückgegeben werden', () => {
+  it('Für einen einzigen Wurf mit 5 soll der Wert 4 zurückgegeben werden', () => {
     expect(electronsAroundTheCores([5])).toBe(4);
   });
 
   it('Für einen Wurf ohne 3/5 (zB [1,2,4,6] soll der Wert 0 zurückgegeben werden', () => {
     expect(electronsAroundTheCores([1,2,4,6]).toBe(0));
+  });
+
+  it('Für einen Wurf [3,3] soll der Wert 4 zurückgegeben werden', () => {
+    expect(electronsAroundTheCores([3,3]).toBe(4));
   });
   
 });
