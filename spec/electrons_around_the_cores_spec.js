ab8bad4c5aea3d56da08ae036b424aee99dcbdcb
Beyza <if24b040@technikum-wien.at>
2025-06-27 17:11:07 +0200

    Failing Test für Einzelwürfel [5] -> 4 schreiben

--- spec/electrons_around_the_cores_spec.js
+++ spec/electrons_around_the_cores_spec.js
@@ -2,4 +2,8 @@ describe('electronsAroundTheCores', () => {
   it('Für einen einzigen Wurf mit 3 soll der Wert 2 zurückgegeben werden', () => {
     expect(electronsAroundTheCores([3])).toBe(2);
   });
+
+  it ('Für einen einzigen Wurf mit 5 soll der Wert 4 zurückgegeben werden', () => {
+    expect(electronsAroundTheCores([5])).toBe(4);
+  });
 });
