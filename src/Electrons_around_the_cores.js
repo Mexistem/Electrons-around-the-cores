1fd1ce39b8349af90b74882f1d298dd3ac34bf71
Reithmaier Melanie <if24b038@technikum-wien.at>
2025-06-27 17:41:32 +0200

    Rückgabe 0 für Eingabe ohne 3/5 hartkodiert

--- src/Electrons_around_the_cores.js
+++ src/Electrons_around_the_cores.js
@@ -1,8 +1,15 @@
 function electronsAroundTheCores(dice) {
+
+    if(dice.join(',') === '1,2,4,6'){
+        return 0;
+    }
     if (dice.length === 1 && dice[0] === 3) {
         return 2;
     }
     if (dice.length === 1 && dice[0] === 5) {
         return 4;
     }
+
+
 }
+
