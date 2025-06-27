ececb50493fa9e0b653c6495957c97cc82bc5081
Beyza <if24b040@technikum-wien.at>
2025-06-27 18:22:02 +0200

    Refactor: Vereinheitlichte Berechnung für beliebige Würfelarrays

--- src/Electrons_around_the_cores.js
+++ src/Electrons_around_the_cores.js
@@ -1,19 +1,6 @@
 function electronsAroundTheCores(dice) {
     const count3 = dice.filter(d => d === 3).length;
-    
-    if (count3 > 0) {
-        return count3 * 2;
-    }
-  
-    if (dice.length === 1 && dice[0] === 5) {
-        return 4;
-    }
-
     const count5 = dice.filter(d => d === 5).length;
 
-    if (count5 > 0){
-        return count5 * 4;
-    }
-  
-    return 0;
-  }
+    return count3 * 2 + count5 * 4;
+}
